98055f50082b6cf7490aaddfdea7ada189e7aa97
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-03-17 21:30:42 -0300

    feat: image validation progress

--- src/javascript/form.js
+++ src/javascript/form.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const email = document.querySelector('#email');
+const imagem = document.querySelector('#image');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -10,6 +11,7 @@ function checkInputs() {
     console.log("Fui chamado");
 
     const emailValue = email.value.trim();
+    const imagemFile = imagem.files[0];
 
     if (emailValue === "") {
         setErrorFor(email, 'O email é obrigatório');
@@ -17,6 +19,16 @@ function checkInputs() {
         setErrorFor(email, 'Insira um email válido')
     } else {
         setSuccessFor(email);
+    }
+
+    if (imagemFile) {
+        if (imagemFile.size > 500 * 1024) {
+            console.log(`Imagem muito grande: ${imagemFile.size / 1024} KB`);
+            setErrorForImage(imagem, 'A imagem deve ser menor que 500KB');
+        } else {
+            console.log(`Imagem válida: ${imagemFile.size / 1024} KB`);
+            setSuccessFor(imagem);
+        }
     }
 }
 
@@ -26,6 +38,16 @@ function setErrorFor(input, message) {
 
     small.innerText = message;
     inputSection.className = 'input-section error';
+}
+
+function setErrorForImage(input, message) {
+    const formInfoSection = document.querySelector('.form_info_section'); 
+    const small = formInfoSection.querySelector('small'); 
+    const img = formInfoSection.querySelector('icon');
+
+    small.innerText = message; 
+    formInfoSection.className = 'form_info_section error'; 
+    
 }
 
 function checkEmail(email) {
